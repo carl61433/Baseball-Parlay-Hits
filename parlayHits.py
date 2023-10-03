6baf5954c101bef39ab1ba447f6a365c5278360e
carl61433 <cd.kopf@gmail.com>
2023-10-02 23:26:43 -0400

    Cleaned up, created a function for dictionary population, and added each created dictionary as a value to a key, which is the filename for the respective stats

--- parlayHits.py
+++ parlayHits.py
@@ -6,26 +6,31 @@ import os
 os.chdir('Players/')
 # Import all file names in the Workbooks dir
 allFiles = os.listdir()
+playerHits = {} # Dictionary to store all players and stats
 print(allFiles)
 
-#def populateDict(allFiles):
-# Open the Workbook
+def populateDict(allFiles, playerHits):
+	print("TEST")
+	# Open the Workbook
+	for name in range(len(allFiles)):
+		player = xlrd.open_workbook(allFiles[name])
 
-for a in range(len(allFiles)):
-	player = xlrd.open_workbook(allFiles[a])
+		# Open the worksheet
+		stats = player.sheet_by_index(0)
 
-	# Open the worksheet
-	stats = player.sheet_by_index(0)
+		# Define the dictionary
+		datesAndHits = {}
 
-	# Define the dictionary
-	datesAndHits = {}
+		# Iterate the rows and columns
+		for row in range(stats.nrows): # Need to check for proper date format, and remove headers
+			if row == 0: # Used to skip the first row of headers
+				row = row + 1
+			datesAndHits[stats.cell_value(row, 3)] = stats.cell_value(row, 12)
+		playerHits[allFiles[name]] = datesAndHits
+		#print(datesAndHits)
+	print(playerHits)
 
-	# Iterate the rows and columns
-	for row in range(stats.nrows): # Need to check for proper date format, and remove headers
-		if row == 0: # Used to skip the first row of headers
-			row = row + 1
-		datesAndHits[stats.cell_value(row, 3)] = stats.cell_value(row, 12)
-	print(datesAndHits)
-
+if __name__=="__main__":
+	populateDict(allFiles, playerHits)
 
 # Eventually need to compare all six players using the matching date from each dict
