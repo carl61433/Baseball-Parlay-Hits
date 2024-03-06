5d2145380bd6bdd32c94d77ee1dc97baec11d783
carl61433 <cd.kopf@gmail.com>
2024-03-06 16:26:42 -0500

    Added a ton of code to the By 2s comparison, just trying to get it working. It is working now, just need to add some comments and clean it up.

--- By 2s Comparison.py
+++ By 2s Comparison.py
@@ -11,6 +11,7 @@ os.chdir('Players/')
 allFiles = [f for f in os.listdir('.') if os.path.isfile(f)] #Only get files, not folders
 playerDictionary = {} #Dictionary to associate player names with their stats
 playerDatesAndHits = {} #Dictionary to keep track of player results, using nested dictionaries
+playerDaysHit = {}
 
 def playerInit(): #Create the dictionary of players, with filename as key and the worksheet as the value
 	for i in allFiles:
@@ -19,6 +20,7 @@ def playerInit(): #Create the dictionary of players, with filename as key and th
 		stats = workbook.sheet_by_index(0)
 		playerDictionary.update({playerName:stats})
 		playerDatesAndHits[playerName] = {}
+		playerDaysHit[playerName] = 0
 	populateDateDict()
 
 def populateDateDict():
@@ -33,6 +35,7 @@ def populateDateDict():
 def playerIteration(date):
 	date = date.strftime("%b %-d") #Modifies the date to XXX ## format used in the data
 	for player in playerDictionary:
+		dayCounter = 0
 		row = 0
 		while row < playerDictionary[player].nrows - 1:
 			gameCheckCounter = 0 #Essentially a boolean to check if an entry was made for that date
@@ -40,6 +43,7 @@ def playerIteration(date):
 				#set the value of the date key to the number of hits
 				if playerDictionary[player].cell_value(row, 12) > 0:
 					playerDatesAndHits[player].update({date:"yes"})
+					dayCounter += 1
 				else:
 					playerDatesAndHits[player].update({date:"no"})
 				gameCheckCounter = 1
@@ -47,6 +51,7 @@ def playerIteration(date):
 			row += 1
 		if gameCheckCounter == 0:
 			playerDatesAndHits[player].update({date:"NG"})
+		playerDaysHit[player] += dayCounter
 
 def numberOfDays(): #The number of times that this occurred in the season.
 	playerCompare = {}
@@ -54,29 +59,38 @@ def numberOfDays(): #The number of times that this occurred in the season.
 		playerCompare.update({i:[]})
 		for j in playerDatesAndHits:
 			counter = 0
+			dayCounter = 0
 			if j == i:
 				playerCompare[i].append("x")
-				break
+				continue
 			seasonDay = datetime.date(2023, 3, 30) #Current day of the season
 			seasonEnd = datetime.date(2023, 9, 28) #Last day of the season
 			while seasonDay <= seasonEnd:
 				date = seasonDay.strftime("%b %-d")
 				if playerDatesAndHits[i][date] == "yes" and playerDatesAndHits[j][date] == "yes":
-					counter = counter + 1
+					counter += 1
 				seasonDay += datetime.timedelta(days=1) #Next day
-			playerCompare[i].append(counter)
+			if j != i:
+				playerCompare[i].append(counter)
+			for item in range(len(playerCompare[i])):
+				if playerCompare[i][item] == "x":
+					break
+				else:
+					playerCompare[i][item] = 0
 	os.chdir('..')
 	with open('By2s.csv', 'w', newline='') as file:
 		playerList = []
 		writer = csv.writer(file)
 		topRow = ['NAME:']
 		topRow += list(playerCompare.keys())
+		topRow += ['# Days']
 		writer.writerow(topRow)
 		for player in playerCompare:
 			playerList = []
 			playerList.append(player)
 			for x in range(len(playerCompare[player])):
 				playerList.append(playerCompare[player][x])
+			playerList.append(playerDaysHit[player])
 			writer.writerow(playerList)
 
 if __name__=="__main__":
